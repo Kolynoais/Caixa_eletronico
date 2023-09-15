611b20f73bfd5405bea7b89e836e8da2bb169253
Kolynoais <145156208+Kolynoais@users.noreply.github.com>
2023-09-15 17:19:02 -0300

    Update caixa_eletronico.py
    Um caixa eletrônico simples

--- caixa_eletronico.py
+++ caixa_eletronico.py
@@ -1,24 +1,65 @@
-TIPO_E="E"
+saldo_da_conta=1000.50
 
-TIPO_N="N"
+exibir_o_extrato_ou_nao="N"
 
-limite=0
+quantidade_de_depositos_diarios=0
 
-print("informe qual e o tipo do cartão,N para normal e E para especial:",end=" ")
+quantidade_de_saques_diarios=0
 
-tipo_do_cartao=input("")
+while exibir_o_extrato_ou_nao=="N":
+    atividade=input("O que deseja fazer? D para deposito(pode fazer 3 diariamente de R$ 500,00) S para saque e E para extrato.")
 
-if tipo_do_cartao in TIPO_N:
-    limite=5000.0
-elif tipo_do_cartao in TIPO_E:
-    limite=100000000.0
+    if atividade=="S":
 
-print("O limite é",limite,"informe o quanto deseja sacar:",end=" ")
+        valor_do_saque=float(input("de quanto é o saque?"))
 
-saque=float(input(""))
+        if valor_do_saque<0:
+            print("saque negativo não é permitido.")
+            exibir_o_extrato_ou_nao="S"
 
-if limite  >= saque:
-    novo_limite=limite-saque
-    print("Transação realizada! O novo limite e de",novo_limite)
+        elif valor_do_saque>saldo_da_conta:
+            print("o valor do saque ultrapassou o saldo da conta.")
+            exibir_o_extrato_ou_nao="S" 
+
+        quantidade_de_saques_diarios=quantidade_de_saques_diarios+1
+
+        if valor_do_saque>0 and valor_do_saque<saldo_da_conta:
+            saldo_da_conta=saldo_da_conta-valor_do_saque
+
+        if valor_do_saque>0 and valor_do_saque<saldo_da_conta:
+            print("sua conta contem R$",saldo_da_conta)
+
+    elif atividade=="D":
+
+        quantidade_do_deposito=float(input("informe a quantidade do deposito (não pode ultrapassar R$ 500,00)."))
+
+        if quantidade_do_deposito<0:
+            print("deposito negativo não é permitido.")
+            exibir_o_extrato_ou_nao="S"
+
+        elif quantidade_do_deposito>500.00:
+            print("deposito passou R$500,00.")
+            exibir_o_extrato_ou_nao="S"
+
+        elif quantidade_de_depositos_diarios>=3:
+            print("limite de depositos alcançado.")
+            exibir_o_extrato_ou_nao="S"
+
+        quantidade_de_depositos_diarios=quantidade_de_depositos_diarios+1
+
+        if quantidade_do_deposito<500.00 and quantidade_do_deposito>0 and quantidade_de_depositos_diarios<=3:
+            saldo_da_conta=saldo_da_conta+quantidade_do_deposito
+
+        if quantidade_do_deposito<500.00 and quantidade_do_deposito>0 and quantidade_de_depositos_diarios<=3:
+            print("sua conta contem R$",saldo_da_conta)
+
+    elif atividade == "E":
+        exibir_o_extrato_ou_nao="S"
 else:
-    print("A transação não foi realizado pois passou do limite definido")
+    print(F""".....................................
+Quantidade de depositos:""",quantidade_de_depositos_diarios,F"""
+.....................................
+quantidade de saques:""",quantidade_de_saques_diarios,F"""
+.....................................
+saldo atual:""",saldo_da_conta)
+    
